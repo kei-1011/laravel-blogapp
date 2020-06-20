01669cc7a3d98b7dc6a92be5600e6c2057107ea8
kei-1011 <k_kojima0407@yahoo.co.jp>
2020-06-21 02:27:27 +0900

    Blog migration

---
+++ database/migrations/2020_06_20_172459_create_following_tables.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFollowingTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('following', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->integer('following_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('following');
+    }
+}
