1aa31fa447d54d0069bef669334cc14092829f19
kei-1011 <k_kojima0407@yahoo.co.jp>
2020-07-19 13:16:10 +0900

    style: レスポンシブスタイル修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,11 +26,11 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
-                </button>
+                </button> --}}
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                {{-- <div class="collapse navbar-collapse" id="navbarSupportedContent"> --}}
                     <ul class="navbar-nav ml-auto">
                         @guest
                             <li class="nav-item">
@@ -74,7 +74,7 @@
                         </form>
                         @endguest
                     </ul>
-                </div>
+                {{-- </div> --}}
             </div>
         </nav>
 
